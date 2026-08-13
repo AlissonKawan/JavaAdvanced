7a6a5bcb0ab11a07fc93a94f3d10cb98988f20f3
AlissonKawan <seuemail@exemplo.com>
2026-08-13 20:30:02 -0300

    começando projeto JavaAdvanced

---
+++ src/main/java/br/com/fiap2tdsps/projectmgt/ProjetoJavaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap2tdsps.projectmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoJavaAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap2tdsps/projectmgt/ProjetoJavaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap2tdsps.projectmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
